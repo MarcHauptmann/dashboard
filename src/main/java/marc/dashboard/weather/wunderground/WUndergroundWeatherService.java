58c1a55cb2dc1810f6d6822a8f185f10f47c4fdc
Marc Hauptmann <marc.hauptmann@stud.uni-hannover.de>
2015-05-01 13:55:43 +0200

    averaging of temperature

--- src/main/java/marc/dashboard/weather/wunderground/WUndergroundWeatherService.java
+++ src/main/java/marc/dashboard/weather/wunderground/WUndergroundWeatherService.java
@@ -16,16 +16,20 @@ import javax.inject.Inject;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.lang.Math.*;
+import static java.util.stream.Collectors.averagingDouble;
 
 @ApplicationScoped
 @Primary
 public class WUndergroundWeatherService implements WeatherService {
 
+    public static final int WINDOW_SIZE = 2;
     @Inject
     private Configuration configuration;
 
@@ -154,8 +158,45 @@ public class WUndergroundWeatherService implements WeatherService {
         return forecasts.stream().collect(Collectors.toMap(this::toDate, Forecast::getRain));
     }
 
+    static class DateDoublePair {
+        Date date;
+        double value;
+
+        public DateDoublePair(Date date, double value) {
+            this.date = date;
+            this.value = value;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public String getDateAsString() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date);
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
+
     @Override
     public Map<Object, Number> getTemparatureForecast() {
-        return forecasts.stream().collect(Collectors.toMap(this::toDate, Forecast::getTemperature));
+        List<DateDoublePair> list = new ArrayList<>();
+
+        for (int i = 0; i < forecasts.size(); i++) {
+            int startIndex = max(0, i - WINDOW_SIZE);
+            int endIndex = min(forecasts.size() - 1, i + WINDOW_SIZE);
+
+            Double temp = forecasts.subList(startIndex, endIndex).stream()
+                    .collect(averagingDouble(Forecast::getTemperature));
+
+            Date date = forecasts.get(i).getTime();
+
+            list.add(new DateDoublePair(date, temp));
+        }
+
+        return list.stream()
+                .collect(Collectors.toMap(DateDoublePair::getDateAsString, DateDoublePair::getValue));
     }
 }
